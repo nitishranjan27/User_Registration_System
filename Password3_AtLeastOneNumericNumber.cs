e8c5c029a9f50dfb4e9537aa37276368d1fec216
nitishranjan27 <65806427+nitishranjan27@users.noreply.github.com>
2022-02-10 10:27:04 +0530

    Nitish Added UC7 Program

---
+++ Password3_AtLeastOneNumericNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration_in_Regex
+{
+	public class Password3_AtLeastOneNumericNumber
+	{
+		public static string REGEX_Password3 = "^[A-Z]{1}[A-Z a-z]{7,}[0-9]{1,}$";  //Nitishkumar12
+
+		public bool ValidatePassword3(string password)
+		{
+			return Regex.IsMatch(password, REGEX_Password3);
+		}
+	}
+}
