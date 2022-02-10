a814bab3a69bdae905476e2fcad6f91d91432ae2
nitishranjan27 <65806427+nitishranjan27@users.noreply.github.com>
2022-02-10 10:31:39 +0530

    Nitish Added UC8 Program

---
+++ Password4_ExactlyOneSpecialCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration_in_Regex
+{
+	public class Password4_ExactlyOneSpecialCharacter
+	{
+		public static string REGEX_Password4 = "^[A-Z]{1}[A-Z a-z]{7,}[!*@#$%^&+=]?[0-9]{1,}$"; //Nitishkumar@1234
+
+		public bool ValidatePassword4(string password)
+		{
+			return Regex.IsMatch(password, REGEX_Password4);
+		}
+	}
+}
