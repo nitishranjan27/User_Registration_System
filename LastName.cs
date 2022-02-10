e32d72e8cded9445434e8707afe5672e72541f71
nitishranjan27 <65806427+nitishranjan27@users.noreply.github.com>
2022-02-10 09:49:06 +0530

    Add files via upload

---
+++ LastName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+/*
+As a User need to enter a valid Last Name
+- Last name starts with Cap and has minimum 3 characters
+*/
+
+namespace User_Registration_System
+{
+	public class LastName
+	{
+		public static string LAST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+
+		public bool ValidateFirstName(string lName)
+		{
+			return Regex.IsMatch(lName, LAST_NAME_REGEX);
+		}
+	}
+}
