f6706d43b995c41ea0193d760698b2f91c574bc3
nitishranjan27 <nk47595@gmail.com>
2022-02-10 09:33:47 +0530

    Nitish Added UC1 Program

---
+++ FirstName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+/*
+As a User need to enter a valid First Name
+- First name starts with Cap and has minimum 3 characters
+*/
+
+namespace User_Registration_System
+{
+	public class FirstName
+	{
+		public static string FIRST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+
+		public bool ValidateFirstName(string fName)
+		{
+			return Regex.IsMatch(fName, FIRST_NAME_REGEX);
+		}
+	}
+}
